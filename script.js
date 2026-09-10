aeb2918d2315e5c36591ae9fa6583cd070ee2efc
zhyarmajed199-rgb <zhyarmajed199@gmail.com>
2026-09-11 00:25:03 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const menuBtn = document.getElementById("menuBtn");
+const nav = document.getElementById("nav");
+
+menuBtn.addEventListener("click", () => {
+  nav.classList.toggle("open");
+});
+
+document.querySelectorAll(".nav a").forEach(link => {
+  link.addEventListener("click", () => {
+    nav.classList.remove("open");
+  });
+});
+
+document.getElementById("year").textContent = new Date().getFullYear();
